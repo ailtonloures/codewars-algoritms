9c6dfed2c3865c58f12b3960aea301e480d4d11c
Ailton Loures <ailton.loures99@gmail.com>
2020-07-06 17:38:47 -0300

    adicionando algoritmos em python

---
+++ python/divisors.py
@@ -0,0 +1,16 @@
+def divisors(integer):
+    i = 2
+    arrDivisors = []
+
+    while i < integer:
+        if integer % i == 0:
+            arrDivisors.append(i)
+        i += 1
+    
+    if len(arrDivisors) == 0:
+        return str(integer) + " is prime"
+
+    return arrDivisors
+
+
+divisors(13)
